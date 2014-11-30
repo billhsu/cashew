3c25b5675354015061a64653747d4992e88d7571
Shipeng Xu <billhsu.x@gmail.com>
2014-11-30 14:39:04 +0800

    adding stateImpl class

---
+++ Xcode/cashew/cashew/Impl/State/StateIdleImpl.h
@@ -0,0 +1,10 @@
+// Shipeng Xu
+// billhsu.x@gmail.com
+
+#pragma once
+#include "Core/State/StateIdle.h"
+
+class StateIdleImpl : public StateIdle
+{
+    void render();
+};
--- src/Core/State/StateIdle.cpp
+++ src/Core/State/StateIdle.cpp
@@ -3,13 +3,16 @@
 
 #include "Core/State/StateIdle.h"
 #include "Core/Camera/Camera.h"
+#include "Core/Controller/Controller.h"
 
 void StateIdle::MouseButton(int button, int state, int x, int y)
-{    
+{
 }
 
 void StateIdle::MouseMotion(int dx, int dy)
 {
+    Controller::rotate.x -= dy;
+    Controller::rotate.y += dx;
 }
 
 void StateIdle::prepareState()
