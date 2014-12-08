0adf9531144cd26180d9c12067a3609f49c1700b
Shipeng Xu <billhsu.x@gmail.com>
2014-12-08 08:50:03 +0800

    modify Quaternion class

--- src/Core/Math/Quaternion.cpp
+++ src/Core/Math/Quaternion.cpp
@@ -14,23 +14,23 @@ Vector3 Quaternion::Z_NEG_AXIS = Vector3(0,0,-1);
 float* Quaternion::getFloat()
 {
     m[ 0] = 1.0f - 2.0f * ( y * y + z * z ); 
-    m[ 1] = 2.0f * ( x * y - z * w );
-    m[ 2] = 2.0f * ( x * z + y * w );
-    m[ 3] = 0;
+    m[ 4] = 2.0f * ( x * y - z * w );
+    m[ 8] = 2.0f * ( x * z + y * w );
+    m[ 12] = 0;
 
-    m[ 4] = 2.0f * (x * y + z * w);
+    m[ 1] = 2.0f * (x * y + z * w);
     m[ 5] = 1.0f - 2.0f * ( x * x + z * z );
-    m[ 6] = 2.0f * ( y * z - x * w );
-    m[ 7] = 0;
+    m[ 9] = 2.0f * ( y * z - x * w );
+    m[ 13] = 0;
 
-    m[ 8] = 2.0f * (x * z - y * w);
-    m[ 9] = 2.0f * (z * y + x * w );
+    m[ 2] = 2.0f * (x * z - y * w);
+    m[ 6] = 2.0f * (z * y + x * w );
     m[10] = 1.0f - 2.0f * ( x * x + y * y );
-    m[11] = 0;
+    m[14] = 0;
 
-    m[12] = 0.0f;
-    m[13] = 0.0f;
-    m[14] = 0.0f;
+    m[ 3] = 0.0f;
+    m[ 7] = 0.0f;
+    m[11] = 0.0f;
     m[15] = 1.0f;
 
     return m;
