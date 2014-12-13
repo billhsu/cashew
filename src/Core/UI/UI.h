17f215ca51eed2e1371aaae4dac43b98c3f52caa
Shipeng Xu <billhsu.x@gmail.com>
2014-12-13 22:10:32 +0800

    add UI system back

---
+++ src/Core/UI/UI.h
@@ -0,0 +1,73 @@
+/*
+ Shipeng Xu
+ billhsu.x@gmail.com
+ 
+ Thanks to GLUI2
+ */
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <stdint.h>
+class LuaTable;
+class UIButton;
+class UINode;
+
+class UI
+{
+public:
+    static UI& getInstance()
+    {
+        static UI instance;
+        std::cout <<"UI getInstance()"<<std::endl;
+        return instance;
+    }
+    
+    void resize(int width, int height);
+    UINode* getNodeByPos(int x, int y);
+    
+    UIButton* addButton(int id, int x, int y, int width, int height,
+                        uint32_t textureID_idle, uint32_t textureID_hover, uint32_t textureID_press,
+                        const char* text, void (*callback)(UINode* Sender), UINode* parent = NULL);
+    
+    UIButton* addButton(int id, const char* strID,
+                        uint32_t textureID_idle, uint32_t textureID_hover, uint32_t textureID_press,
+                        const char* text, void (*callback)(UINode* Sender), UINode* parent = NULL);
+    
+    UINode* MouseButton(int button, int state, int x, int y);
+    UINode* PassiveMotion(int x, int y);
+    
+    static char uiHintText[128];
+    static int hintTextPosX, hintTextPosY;
+    enum{CASHEW_MOUSE_DOWN, CASHEW_MOUSE_UP, CASHEW_RIGHT_BUTTON, CASHEW_LEFT_BUTTON};
+    virtual void render(float timeDelta){}
+    static int getWindowWidth()
+    {
+        return mWindowWidth;
+    }
+    static int getWindowHeight()
+    {
+        return mWindowHeight;
+    }
+    static int getMouseX()
+    {
+        return mMouseX;
+    }
+    static int getMouseY()
+    {
+        return mMouseY;
+    }
+private:
+    UI();
+    ~UI();
+    static int mWindowWidth, mWindowHeight;
+    static int mMouseX, mMouseY;
+    std::vector <UINode*> nodeList;
+    UINode* mRootNode;
+    int luaGetNodePosX(const char *nodeName);
+    int luaGetNodePosY(const char *nodeName);
+    int luaGetNodeWidth(const char *nodeName);
+    int luaGetNodeHeight(const char *nodeName);
+    LuaTable *UILayout;
+    
+};
---
+++ src/Core/UI/UIButton.h
@@ -0,0 +1,24 @@
+/*
+ Shipeng Xu
+ billhsu.x@gmail.com
+ */
+#pragma once
+
+#include "UINode.h"
+
+class UIButton : public UINode
+{
+public:
+    UIButton(UINode* parent);
+    ~UIButton();
+    
+    void MouseButton(int button, int state, int x, int y);
+    void PassiveMotion(int x, int y);
+    
+    void setText(const char* text);
+    void update(float timeDelta);
+    virtual void render(float timeDelta){}
+    
+    uint32_t textureID_idle, textureID_hover, textureID_press;
+private:
+};
