912fb0bb035a941c0ae04db4f286ffb94567ca5c
Shipeng Xu <billhsu.x@gmail.com>
2015-05-26 22:54:27 +0800

    update FontRenderer

--- src/OpenGL/Fonts/FontRenderer.cpp
+++ src/OpenGL/Fonts/FontRenderer.cpp
@@ -1,1 +1,8 @@
+// Shipeng Xu
+// billhsu.x@gmail.com
 #include "FontRenderer.h"
+
+FontRenderer::FontRenderer() {
+    struct sth_stash* stash = sth_create(512, 512);
+    int font = sth_add_font(stash, "/Library/Fonts/Arial Black.ttf");
+}
--- src/OpenGL/Fonts/FontRenderer.h
+++ src/OpenGL/Fonts/FontRenderer.h
@@ -1,1 +1,14 @@
+// Shipeng Xu
+// billhsu.x@gmail.com
 #pragma once
+#include "fontstash.h"
+class FontRenderer {
+public:
+    static FontRenderer& getInstance() {
+        static FontRenderer instance;
+        return instance;
+    }
+private:
+    FontRenderer();
+    struct sth_stash* stash;
+};
