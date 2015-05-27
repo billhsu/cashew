c33affd03f487c8782fce484e31a6e679ff5a73d
Shipeng Xu <billhsu.x@gmail.com>
2015-05-27 23:29:42 +0800

    update FontRenderer class

--- src/OpenGL/Fonts/FontRenderer.cpp
+++ src/OpenGL/Fonts/FontRenderer.cpp
@@ -3,6 +3,24 @@
 #include "FontRenderer.h"
 
 FontRenderer::FontRenderer() {
+    fontList.clear();
     struct sth_stash* stash = sth_create(512, 512);
     int font = sth_add_font(stash, "/Library/Fonts/Arial Black.ttf");
+    fontList["Arial Black"] = font;
+}
+
+void FontRenderer::addText(int fontId, int fontSize, int x, int y, std::string content) {
+    TextRenderInfo textRenderInfo;
+    textRenderInfo.fontId = fontId;
+    textRenderInfo.fontSize = fontSize;
+    textRenderInfo.x = x;
+    textRenderInfo.y = y;
+    textRenderInfo.content = content;
+    textRenderInfoList.push_back(textRenderInfo);
+}
+
+void FontRenderer::render(float timeDelta) {
+    // TODO RENDER
+    
+    textRenderInfoList.clear();
 }
--- src/OpenGL/Fonts/FontRenderer.h
+++ src/OpenGL/Fonts/FontRenderer.h
@@ -1,14 +1,32 @@
 // Shipeng Xu
 // billhsu.x@gmail.com
 #pragma once
-#include "fontstash.h"
+#include <map>
+#include <string>
+#include <vector>
+#include <OpenGL/gl3.h>
+
+extern "C" {
+    #include "fontstash.h"
+}
 class FontRenderer {
 public:
     static FontRenderer& getInstance() {
         static FontRenderer instance;
         return instance;
     }
+    struct TextRenderInfo {
+        int fontId;
+        int fontSize;
+        int x;
+        int y;
+        std::string content;
+    };
+    void addText(int fontId, int fontSize, int x, int y, std::string content);
+    void render(float timeDelta);
 private:
     FontRenderer();
     struct sth_stash* stash;
+    std::map <std::string, int> fontList;
+    std::vector<TextRenderInfo> textRenderInfoList;
 };
