5e261189ea67607a1c1b5341b93de021c1698227
Shipeng Xu <billhsu.x@gmail.com>
2015-11-01 22:10:37 +0800

    remove libcashew folder

--- libcashew/src/cashew/scene/LineSegment.cpp
+++
@@ -1,5 +0,0 @@
-#include "cashew/scene/LineSegment.h"
-
-void LineSegment::render(float r, float g, float b)
-{
-}
--- libcashew/src/cashew/texture/Texture.cpp
+++
@@ -1,7 +0,0 @@
-#include "cashew/texture/Texture.h"
-using namespace cashew;
-
-uint32_t Texture::loadTexture(std::string fileName)
-{
-    return 0;
-}
