24f8facc8c0100f863468bcb9cb98739989e3f60
Shipeng Xu <billhsu.x@gmail.com>
2015-11-29 18:17:30 +0800

    add fog

--- Shader/lineSegment.fs
+++ Shader/lineSegment.fs
@@ -2,12 +2,24 @@
 // billhsu.x@gmail.com
 
 #version 330 core
+#define FOG_DENSITY 0.05
 
 uniform sampler2D PeelLayerDepthMap;
 
 in vec4 colorGs;
 
 out vec4 color;
+
+// https://github.com/hughsk/glsl-fog/blob/master/exp2.glsl
+float fogFactorExp2(
+                    const float dist,
+                    const float density
+                    ) {
+    const float LOG2 = -1.442695;
+    float d = density * dist;
+    return 1.0 - clamp(exp2(d * d * LOG2), 0.0, 1.0);
+}
+
 void main (void)
 {
     float z = texelFetch(PeelLayerDepthMap, ivec2(gl_FragCoord.xy), 0).x;
@@ -17,5 +29,9 @@ void main (void)
     if (colorGs.a<0.1) {
         discard;
     }
-    color = colorGs;
+    float fogDistance = gl_FragCoord.z / gl_FragCoord.w;
+    float fogAmount = fogFactorExp2(fogDistance, FOG_DENSITY);
+    vec4 fogColor = vec4(colorGs.xyz, 0.3f);
+    color = mix(colorGs, fogColor, fogAmount);
 }
+
--- src/OpenGL/Shader/GLSLShader.cpp
+++ src/OpenGL/Shader/GLSLShader.cpp
@@ -7,88 +7,80 @@
 int GLSLShader::currentShaderProgramID = 0;
 GLSLShader* GLSLShader::currentShaderProgramObj = 0;
 
-GLSLShader::GLSLShader()
-{
-    mProgramID=-1;
+GLSLShader::GLSLShader() {
+    mProgramID = -1;
     mNumShaders = 0;
     mShaders[0] = 0;
     mShaders[1] = 0;
     mShaders[2] = 0;
 }
 
-GLSLShader::~GLSLShader()
-{
-    std::cout<<"~GLSLShader() "<<mProgramID<<std::endl;
+GLSLShader::~GLSLShader() {
+    std::cout << "~GLSLShader() " << mProgramID << std::endl;
     glDeleteProgram(mProgramID);
-    mProgramID=-1;
+    mProgramID = -1;
 }
 
-void GLSLShader::loadFromFile(GLenum shader_type, const std::string& filename)
-{
-    std::cout<<"Loading shader file: "<<filename<<std::endl;
+void GLSLShader::loadFromFile(GLenum shader_type, const std::string& filename) {
+    std::cout << "Loading shader file: " << filename << std::endl;
     std::ifstream shaderFile(filename);
-    if (shaderFile.is_open())
-    {
+    if (shaderFile.is_open()) {
         std::string line, fileContent;
-        while ( getline (shaderFile,line) )
-        {
+        while (getline(shaderFile, line)) {
             fileContent.append(line);
             fileContent.append("\r\n");
         }
         shaderFile.close();
         loadFromString(shader_type, fileContent);
-    }
-    else std::cerr << "Unable to open file "<<filename<<std::endl;
+    } else
+        std::cerr << "Unable to open file " << filename << std::endl;
 }
 
-void GLSLShader::loadFromString(GLenum shader_type, const std::string& shaderContent)
-{
+void GLSLShader::loadFromString(GLenum shader_type,
+                                const std::string& shaderContent) {
     GLuint shader = glCreateShader(shader_type);
-    
-    const char * ptmp = shaderContent.c_str();
-    glShaderSource (shader, 1, &ptmp, NULL);
-    
+
+    const char* ptmp = shaderContent.c_str();
+    glShaderSource(shader, 1, &ptmp, NULL);
+
     GLint status;
-    glCompileShader (shader);
-    glGetShaderiv (shader, GL_COMPILE_STATUS, &status);
+    glCompileShader(shader);
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
     if (status == GL_FALSE) {
         GLint infoLogLength;
-        glGetShaderiv (shader, GL_INFO_LOG_LENGTH, &infoLogLength);
-        GLchar *infoLog= new GLchar[infoLogLength];
-        glGetShaderInfoLog (shader, infoLogLength, NULL, infoLog);
-        std::cerr<<"Compile log: "<<infoLog<<std::endl;
-        delete [] infoLog;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
+        GLchar* infoLog = new GLchar[infoLogLength];
+        glGetShaderInfoLog(shader, infoLogLength, NULL, infoLog);
+        std::cerr << "Compile log: " << infoLog << std::endl;
+        delete[] infoLog;
     }
-    mShaders[mNumShaders++]=shader;
+    mShaders[mNumShaders++] = shader;
 }
 
-void GLSLShader::createProgram()
-{
-    mProgramID = glCreateProgram ();
-    for(int i=0; i<mNumShaders; ++i)
-    {
+void GLSLShader::createProgram() {
+    mProgramID = glCreateProgram();
+    for (int i = 0; i < mNumShaders; ++i) {
         if (mShaders[i] != 0) {
-            glAttachShader (mProgramID, mShaders[i]);
+            glAttachShader(mProgramID, mShaders[i]);
         }
     }
-    
+
     GLint status;
-    glLinkProgram (mProgramID);
-    glGetProgramiv (mProgramID, GL_LINK_STATUS, &status);
+    glLinkProgram(mProgramID);
+    glGetProgramiv(mProgramID, GL_LINK_STATUS, &status);
     if (status == GL_FALSE) {
         GLint infoLogLength;
-        
-        glGetProgramiv (mProgramID, GL_INFO_LOG_LENGTH, &infoLogLength);
-        GLchar *infoLog= new GLchar[infoLogLength];
-        glGetProgramInfoLog (mProgramID, infoLogLength, NULL, infoLog);
-        std::cerr<<"Link log: "<<infoLog<<std::endl;
-        delete [] infoLog;
+
+        glGetProgramiv(mProgramID, GL_INFO_LOG_LENGTH, &infoLogLength);
+        GLchar* infoLog = new GLchar[infoLogLength];
+        glGetProgramInfoLog(mProgramID, infoLogLength, NULL, infoLog);
+        std::cerr << "Link log: " << infoLog << std::endl;
+        delete[] infoLog;
     }
-    for(int i=0; i<mNumShaders; ++i)
-    {
+    for (int i = 0; i < mNumShaders; ++i) {
         glDeleteShader(mShaders[i]);
     }
-    std::cout<<"createProgram() "<<mProgramID<<std::endl;
+    std::cout << "createProgram() " << mProgramID << std::endl;
 }
 
 void GLSLShader::bind() {
